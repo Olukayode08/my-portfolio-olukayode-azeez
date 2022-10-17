0c2fe1e572541be164dd8106e1840f791c44fbd6
Azeez Olukayode <Olukayodeazeez2000@gmail.com>
2022-10-17 11:28:47 +0100

    Intro

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React from "react";
 import Hero from "./components/Hero";
-import Navbar from "./components/Navbar";
+import Navbar from './components/Navbar';
+import Skills from './components/Skills';
+import Contact from './components/Contact';
 
 
 function App() {
@@ -8,6 +10,8 @@ function App() {
     <>
     <Hero />
     <Navbar />
+    <Skills />
+    <Contact />
     
     </>
   )
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,5 +1,10 @@
 import React from 'react';
 import styled from 'styled-components';
+import Cv from '../assets/Olukayode.pdf';
+import { Link } from 'react-scroll';
+import { FaGithub, FaLinkedinIn } from 'react-icons/fa';
+import { GrTwitter } from 'react-icons/gr';
+
 
 const Hero = () => {
   return (
@@ -7,12 +12,84 @@ const Hero = () => {
       <section id='hero'>
         <Wrapper>
           <main>
-            <h1>Hero</h1>
+            <h3>Hello I'm</h3>
+            <h4>Olukayode Azeez</h4>
+            <h3>A Frontend Developer</h3>
+            <div className='btn-container'>
+              <a className='links' href={Cv} download>
+                Download Cv
+              </a>
+              <Link
+                className='links'
+                to='contact'
+                spy={true}
+                smooth={true}
+                offset={50}
+                duration={500}
+              >
+                Let's Talk
+              </Link>
+            </div>
           </main>
+          <div className='socials'>
+            <a href='https://github.com/Olukayode08' target='_blank'>
+              <FaGithub />
+            </a>
+            <a
+              href='https://www.linkedin.com/in/olukayode-azeez-a09b94238/'
+              target='_blank'
+            >
+              <FaLinkedinIn />
+            </a>
+            <a href='https://twitter.com/_jireazeez' target='_blank'>
+              <GrTwitter />
+            </a>
+          </div>
         </Wrapper>
       </section>
     </>
   );
 };
-const Wrapper = styled.section``;
+const Wrapper = styled.section`
+  position: relative;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  height: 100vh;
+  text-align: center;
+  h3 {
+    font-size: 45px;
+  }
+  h4 {
+    font-size: 60px;
+  }
+  .btn-container {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+  }
+  .links {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    width: 120px;
+    height: 40px;
+    margin: 0 7px;
+    text-decoration: none;
+    color: #000;
+    background: #fff;
+    cursor: pointer;
+    border-radius: 10px;
+  }
+  .socials{
+    position: absolute;
+    bottom: 150px;
+    left: 10px;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+  }
+`;
 export default Hero;
