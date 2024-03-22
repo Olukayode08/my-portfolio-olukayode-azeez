b2deb16a07ea772e072ae29635e97913a39148bc
Olukayode <olukayodeazeez2000@gmail.com>
2024-03-22 21:51:13 +0100

    New Project

--- src/data.js
+++ src/data.js
@@ -1,44 +1,51 @@
 import lendsqr from './assets/IMG_0130.jpg'
-import cardtonic from './assets/IMG_0133.jpg'
 import ebike from './assets/IMG_0134.jpg'
 import github from './assets/github.png'
 import feedback from './assets/feedback.png'
 import atunlo from './assets/atunlo.png'
 import transfy from './assets/transfy.png'
-import hotel from './assets/IMG_0137.jpg'
 import foodApp from './assets/foodApp.jpeg'
+import mandate from './assets/firstmandate.png'
+
 
 export const projects = [
   {
     id: 1,
+    img: mandate,
+    text: '1st Mandate is Property Dashboard for Landlords, Property Mangers and Tenants. Web application still in progress',
+    github: 'https://github.com/Olukayode08/First-Mandate-Components',
+    demo: 'https://apptest.1stmandate.com/',
+  },
+  {
+    id: 2,
     img: github,
     text: 'Explored the Github API to search different Github Developers.',
     github: 'https://github.com/Olukayode08/Search-Github-Users',
     demo: 'https://github-finder-app-rose-two.vercel.app/',
   },
   {
-    id: 2,
+    id: 3,
     img: transfy,
     text: 'A user form validation built with React and Firebase',
     github: 'https://github.com/Olukayode08/Transfy-Authentification',
     demo: 'https://transfy-authentification.vercel.app/',
   },
   {
-    id: 3,
+    id: 4,
     img: foodApp,
     text: 'An e-commerce food application, ',
     github: 'https://github.com/Olukayode08/ecommerce-food-app',
     demo: 'https://ecommerce-food-app.vercel.app/',
   },
   {
-    id: 4,
+    id: 5,
     img: atunlo,
     text: 'Atunlo. Built with react, styled-components, react-router-dom ',
-    github: '#',
-    demo: 'https://atunlotech.com/',
+    github: 'https://github.com/Olukayode08/atunlo',
+    demo: 'https://atunlo.vercel.app/',
   },
   {
-    id: 5,
+    id: 6,
     img: lendsqr,
     text: 'Lendsqr Users admin dashboard using React and SCSS.',
     github: 'https://github.com/Olukayode08/olukayode-azeez-lendsqr-fe-test',
@@ -51,26 +58,11 @@ export const projects = [
     github: 'https://github.com/Olukayode08/Feedback-application',
     demo: 'https://feedback-application-iota.vercel.app/',
   },
-
-  {
-    id: 7,
-    img: cardtonic,
-    text: 'A clone of Cardtonic Webpage using React.',
-    github: 'https://github.com/Olukayode08/Cardtonic-landing-page',
-    demo: 'https://cardtonic-landing-page.vercel.app/',
-  },
   {
     id: 8,
     img: ebike,
     text: 'An ebike landing page built with React and TailwindCSS',
     github: 'https://github.com/Olukayode08/ebike-landing-page',
     demo: 'https://ebike-landing-page.vercel.app/',
-  },
-  {
-    id: 9,
-    img: hotel,
-    text: 'An hotel webpage built with React.',
-    github: 'https://github.com/Olukayode08/real-estate',
-    demo: 'https://real-estate-opal.vercel.app/',
   },
 ]
